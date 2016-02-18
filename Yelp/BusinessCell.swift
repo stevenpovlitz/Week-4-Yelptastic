9d99eac527f8735f7b331233c943fa13fbca449e
Steven Povlitz <steven.povlitz@gmail.com>
2016-02-18 00:49:05 -0500

    completed video 2

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -39,6 +39,15 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
